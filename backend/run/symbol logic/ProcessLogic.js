3f99fb87c9907a9aae6f953b5086785c20313b1f
Gio Dela Cruz <61963067+GGnagaraxx@users.noreply.github.com>
2022-06-12 18:33:57 +0800

    Process symbol logic

--- backend/run/symbol logic/ProcessLogic.js
+++ backend/run/symbol logic/ProcessLogic.js
@@ -1,3 +1,6 @@
+import Calculator from "../logic functions/Calculator.js";
+import Concatenator from "../logic functions/Concatenator.js";
+
 const validValues = {
     ConstantsAndVariables: [
         "Identifier",
@@ -18,23 +21,58 @@ export default function ProcessLogic(groupedTokens, flowgram){
     }
 
     let groupedClone = [...groupedTokens];
+    let varInfo = {
+        variableName: null,
+        value: null,
+        type: null
+    }
 
 
     let state = 0;                          //0 - variable name, 1 - equal sign, 2 - value (constants, mathematical op, concatenation, another variable)
     while(groupedClone.length > 0){
         if (state == 0 && groupedClone[0].Type == "Identifier"){
-
+            varInfo.variableName = groupedClone[0].Token
+            groupedClone.shift()
         } else if(state == 1 && groupedClone[0].Type == "Assign Operator"){
-
+            groupedClone.shift()
         } else if(state == 2){
-            if(validValues.ConstantsAndVariables.includes(groupedClone[0].Type)){                   // Consant or variable
-
+            if(validValues.ConstantsAndVariables.includes(groupedClone[0].Type)){                   // Constant or variable
+                if(groupedClone[0].Type == "Identifier"){
+                    const existingVar = flowgram.getVariable(groupedClone[0].Token);
+                    if(existingVar){
+                        varInfo.value = existingVar.value
+                        varInfo.type = existingVar.type
+                        groupedClone.shift()
+                    }
+                }
             } else if(validValues.ValidGroupedTypes[0] == groupedClone[0].groupedTokensType){       //Mathematical Operation
+                const ret = Calculator(groupedClone[0].groupedTokens, flowgram);
+                if(ret.error){
+                    res.error = ret.error;
+                    return res;
+                }
+
+                varInfo.value = ret.result.Token
+                varInfo.type = ret.result.Type
+                groupedClone.shift()
 
             } else if(validValues.ValidGroupedTypes[1] == groupedClone[0].groupedTokensType){       //Concatenation
-                
+                const ret = Concatenator(groupedClone[0].groupedTokens, flowgram);
+                if(ret.error){
+                    res.error = ret.error;
+                    return res;
+                }
+
+                varInfo.value = ret.result.Token
+                varInfo.type = ret.result.Type
+                groupedClone.shift()
+            }
+
+            if(varInfo.variableName && varInfo.value != null && varInfo.type){
+                flowgram.addVariable(varInfo.variableName, varInfo.value, varInfo.type);
             }
         }
     }
 
+    return res;
 }
