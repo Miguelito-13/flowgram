a6ecd94a5c2e039c582a8945939526e0f1010582
Gio Dela Cruz <61963067+GGnagaraxx@users.noreply.github.com>
2022-06-20 00:57:16 +0800

    Auto-generated Arrows

---
+++ frontend/connectorsvgs.js
@@ -0,0 +1,31 @@
+const ConnectorSVGs = {
+    left_up_arrow: "./ThesisSVG/arrow left top.svg",
+    left_right_arrow: "./ThesisSVG/arrow right.svg",
+    left_down_arrow: "./ThesisSVG/arrow left bottom.svg",
+    left_up: "./ThesisSVG/plain top left.svg",
+    left_right: "./ThesisSVG/plain horizontal.svg",
+    left_down: "./ThesisSVG/plain bottom left.svg",
+    
+    right_up_arrow: "./ThesisSVG/arrow right top.svg",
+    right_left_arrow: "./ThesisSVG/arrow left.svg",
+    right_down_arrow: "./ThesisSVG/arrow right bottom.svg",
+    right_up: "./ThesisSVG/plain top right.svg",
+    right_left: "./ThesisSVG/plain horizontal.svg",
+    right_down: "./ThesisSVG/plain bottom right.svg",
+    
+    up_left_arrow: "./ThesisSVG/arrow top left.svg",
+    up_down_arrow: "./ThesisSVG/arrow down.svg",
+    up_right_arrow: "./ThesisSVG/arrow top right.svg",
+    up_left: "./ThesisSVG/plain top left.svg",
+    up_down: "./ThesisSVG/plain vertical.svg",
+    up_right: "./ThesisSVG/plain top right.svg",
+    
+    down_left_arrow: "./ThesisSVG/arrow bottom left.svg",
+    down_top_arrow: "./ThesisSVG/arrow up.svg",
+    down_right_arrow: "./ThesisSVG/arrow bottom right.svg",
+    down_left: "./ThesisSVG/plain bottom left.svg",
+    down_top: "./ThesisSVG/plain vertical.svg",
+    down_right: "./ThesisSVG/plain bottom right.svg",
+}
+
+export default ConnectorSVGs;
--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,15 @@
 
-import startRunCompile, {createFlowgram} from '../backend/main.js'
+import startRunCompile, {createFlowgram} from '../backend/main.js';
+import ConnectorSVGs from './connectorsvgs.js';
 
 
 let flowgram = createFlowgram("New Flowgram");
 let htmlSymbols = [];                   // {htmlSymbol: <html object>, backendSymbol: <symbol object>}
+let htmlConnectors = [];                // [gridIDs]
+let gridsInfo = {};                     // [gridID] = {type: "symbol"/"connector", index: arrayIndex}
+let selectedGrids = [];
+
+//=================================================//
 
 let imgElement = new Image();
 let dragElement = document.createElement('span');
@@ -19,6 +25,8 @@ let zoomLevel = 1;
 let x = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let output = document.getElementById("output");
 let zoom = document.getElementById("zoom");
+
+//=================================================//                 Initialize Event listeners
 
 document.addEventListener('DOMContentLoaded', () => {
   
@@ -40,6 +48,8 @@ document.addEventListener('DOMContentLoaded', () => {
   dragElement.classList.add('wheeeee');
   document.querySelector('footer>p').appendChild(dragElement);
 });
+
+//=================================================//                 EventListener Functions
 
 //Gets triggered as soon as the user starts dragging
 function handleDragStart(ev) {
@@ -82,8 +92,9 @@ function handleDrop(ev) {
   let data = JSON.parse(ev.dataTransfer.getData('application/json'));
   let draggable = document.querySelector(`[data-ts="${data.timestamp}"]`);
   let clone = draggable.cloneNode(true);
-  clone.classList.add("samp");
+  clone.classList.add("deletable");
   clone.classList.add("active");
+  clone.classList.add("symbol");
   clone.classList.add("symbol-text");
   dropzone.append(clone);
   // draggable.remove();
@@ -138,14 +149,16 @@ function handleLeave(ev) {
   // console.log('dragleave dropzone');
 }
 
-function handleClick(ev) { 
+//Gets triggered when the user clicks
+function handleClick(ev) {
   // FOR DELETING A SYMBOL
+  console.log(ev.target, ev.target.parentNode)
   if(document.querySelector("button#delete.active")!=null){
     let symbol = ev.target;
-    if(symbol.parentNode.classList.contains('samp')){
+    if(symbol.parentNode.classList.contains('deletable')){
       symbol.remove();
     } 
-    if(symbol.classList.contains('samp')){
+    if(symbol.classList.contains('deletable')){
       symbol.remove();
     } else return;
     // console.log(symbol);
@@ -153,16 +166,11 @@ function handleClick(ev) {
 
   // CONNECTOR FUNCTION (SELECTING SYMBOL FIRST)
   if(document.querySelector("button#connectors.active")!=null){
-    let dropzone = ev.target
-    if(dropzone.parentNode.classList.contains('samp')){
-      let drop = dropzone.parentNode;
-      console.log(drop.parentNode);
-    }
-    if(dropzone.classList.contains('samp')){
-      console.log(dropzone.parentNode);
-    }
+    handleSelectPath(ev);
   }
 }
+
+//=================================================//       Initialize grid
 
 for(let i=0;i<26;i++){ // CREATE GRID WITH ID
   for(let j=1;j<6;j++){
@@ -170,12 +178,23 @@ for(let i=0;i<26;i++){ // CREATE GRID WITH ID
     newElement.classList.add('dropzone');
     newElement.id = x[i] +'0'+ j;
     if(i == 0 && j == 3){
-      newElement.innerHTML = '<input type="text" disabled id="start" name="decision" value="START" data-ts="1655295083726">';
+      newElement.innerHTML = '<input type="text" class="symbol" disabled id="start" name="decision" value="START" data-ts="1655295083726">';
     }
     // newElement.innerHTML = '<p>'+x[i]+'0'+j+'</p>';
     canvas.appendChild(newElement);
+    htmlSymbols.push({
+      htmlSymbol: newElement,
+      backendSymbol: flowgram.main.start
+    });
+
+    gridsInfo[newElement.id] = {
+      type: "symbol",
+      index: htmlSymbols.length - 1
+    }
   }
 }
+
+//=================================================//       Separated eventlisteners
 
 // FOR DOWNLOADING FLOWCHART AS IMAGE
 let save = document.getElementById("save");
@@ -197,25 +216,35 @@ newFlowgram.addEventListener('click', () => {
 let runButton = document.getElementById("runButton");
 runButton.addEventListener('click', () => {
   console.log("Run Button Pressed");
+  startRunCompile(flowgram);
 })
 
 // STOP BUTTON FUNCTION
 let stopButton = document.getElementById("stopButton");
 stopButton.addEventListener('click', () => {
   console.log("Stop Button Pressed");
+  flowgram.status.run = false;
 })
 
 // CONNECTOR BUTTON FUNCTION
 let connectors = document.getElementById("connectors");
 connectors.addEventListener('click', () => {
+  let arrow = document.getElementById("arrow");
   if(connectors.classList.contains("active")){
     connectors.classList.remove("active");
+
+    let selGrids = document.getElementsByClassName('selected-grid');
+    console.log(selGrids, selGrids.length)
+    while(selGrids.length > 0){
+      selGrids.item(0).classList.remove('selected-grid');
+    }
+    selectedGrids = [];
 
     let confirm = document.getElementById("confirm");
     confirm.remove();
     let cancel = document.getElementById("cancel");
     cancel.remove();
-  }else {
+  }else if(!connectors.classList.contains("active") && arrow.classList.contains("active")) {
     connectors.classList.add("active");
 
     // CREATE CONFIRM AND CANCEL BUTTON
@@ -252,6 +281,8 @@ connectors.addEventListener('click', () => {
   // console.log("connector pressed");
 })
 
+//=================================================//
+
 // FOR MODAL
 let modal = document.getElementById("helpModal");
 let helpBtn = document.getElementById("help");
@@ -271,5 +302,118 @@ span.onclick = function(){
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+//=================================================//                   Separated functions
+
+function handleSelectPath(ev){
+  let grid
+  if(ev.target.classList.contains('dropzone')){
+    grid = ev.target
+  } else if(ev.target.parentNode.classList.contains('dropzone')){
+    grid = ev.target.parentNode
+  } else if(ev.target.parentNode.classList.contains('symbol') || ev.target.parentNode.classList.contains('arrow')){
+    grid = ev.target.parentNode.parentNode;
+  }
+
+  let childNode = grid.firstElementChild
+  if(selectedGrids.length == 0 && childNode && childNode.classList.contains('symbol')){
+    grid.classList.add('selected-grid');
+    selectedGrids.push(grid.id);
+
+  } else if(selectedGrids.length > 0){
+    let lastID = selectedGrids[selectedGrids.length-1];
+    let gridID = grid.id;
+
+    
+    if(grid.classList.contains('selected-grid') && grid.id == lastID){        // Deselecting last selected grid
+      grid.classList.remove('selected-grid');
+      selectedGrids.pop()
+    } else if(grid.classList.contains('selected-grid')){
+      return;
+    }
+
+    if((gridID.charCodeAt(0) == lastID.charCodeAt(0) + 1 && gridID.slice(1,3) == lastID.slice(1,3)) ||    //Grid below the last selected
+      (gridID.charCodeAt(0) == lastID.charCodeAt(0) - 1 && gridID.slice(1,3) == lastID.slice(1,3))  ||    //Grid above the last selected
+      (gridID.charAt(0) == lastID.charAt(0) && parseInt(gridID.slice(1,3)) == parseInt(lastID.slice(1,3)) + 1)  ||            //Grid to the right the last selected
+      (gridID.charAt(0) == lastID.charAt(0) && parseInt(gridID.slice(1,3)) == parseInt(lastID.slice(1,3)) - 1)                //Grid to the left the last selected
+    ){     
+      grid.classList.add('selected-grid');
+      selectedGrids.push(gridID);
+
+      if(childNode != null){
+        if(childNode.classList.contains('grid-connector')){
+          let existingArray = htmlConnectors[gridsInfo[gridID].index];
+          selectedGrids[selectedGrids.length - 1] = existingArray[existingArray.length - 1];
+        }
+
+        generateConnectors();
+      }
+    }
+  }
+}
+
+
+function generateConnectors(){
+  let pathCopy = [...selectedGrids];
+  selectedGrids = [];
+
+  if(connectors.classList.contains('active')){
+    connectors.classList.remove('active');
+
+    let confirm = document.getElementById('confirm');
+    confirm.remove();
+    let cancel = document.getElementById('cancel');
+    cancel.remove();
+  }
+
+  for(let i = 1; i < pathCopy.length-1; i++){
+    let from = "";
+    let to = "";
+    let arrow = "";
+
+    if(pathCopy[i-1].charCodeAt(0) == pathCopy[i].charCodeAt(0) + 1 && pathCopy[i-1].slice(1,3) == pathCopy[i].slice(1,3)){
+      from = "down";
+    } else if(pathCopy[i-1].charCodeAt(0) == pathCopy[i].charCodeAt(0) - 1 && pathCopy[i-1].slice(1,3) == pathCopy[i].slice(1,3)){
+      from = "up";
+    } else if(pathCopy[i-1].charAt(0) == pathCopy[i].charAt(0) && parseInt(pathCopy[i-1].slice(1,3)) == parseInt(pathCopy[i].slice(1,3)) + 1){
+      from = "right";
+    } else if(pathCopy[i-1].charAt(0) == pathCopy[i].charAt(0) && parseInt(pathCopy[i-1].slice(1,3)) == parseInt(pathCopy[i].slice(1,3)) - 1){
+      from = "left";
+    }
+
+    if(pathCopy[i+1].charCodeAt(0) == pathCopy[i].charCodeAt(0) + 1 && pathCopy[i+1].slice(1,3) == pathCopy[i].slice(1,3)){
+      to = "down";
+    } else if(pathCopy[i+1].charCodeAt(0) == pathCopy[i].charCodeAt(0) - 1 && pathCopy[i+1].slice(1,3) == pathCopy[i].slice(1,3)){
+      to = "up";
+    } else if(pathCopy[i+1].charAt(0) == pathCopy[i].charAt(0) && parseInt(pathCopy[i+1].slice(1,3)) == parseInt(pathCopy[i].slice(1,3)) + 1){
+      to = "right";
+    } else if(pathCopy[i+1].charAt(0) == pathCopy[i].charAt(0) && parseInt(pathCopy[i+1].slice(1,3)) == parseInt(pathCopy[i].slice(1,3)) - 1){
+      to = "left";
+    }
+
+    if(i == pathCopy.length - 2){
+      arrow = "arrow"
+    }
+
+    let grid = document.getElementById(pathCopy[i]);
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('arrow')
+    let newImg = document.createElement('img')
+    newImg.classList.add('grid-connector')
+    newImg.classList.add('connector-arrow')
+    newImg.id = from + "-" + to
+    newImg.setAttribute('src', ConnectorSVGs[from + "_" + to + (arrow != "" ? "_" + arrow: "")]);
+    grid.appendChild(newDiv);
+    newDiv.appendChild(newImg);
+
+
+    let selGrids = document.getElementsByClassName('selected-grid');
+    while(selGrids.length > 0){
+      selGrids.item(0).classList.remove('selected-grid');
+    }
+    selectedGrids = [];
+
   }
 }
